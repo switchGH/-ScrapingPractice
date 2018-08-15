b5938eddbff009475dfcb19fc35bd522a59f7036
switch <shg18.sk1.hr10@gmail.com>
2018-08-15 23:32:44 +0900

    htmlファイルとして保存する機能を追加

--- practice1/practice1.py
+++ practice1/practice1.py
@@ -1,3 +1,7 @@
 import requests
-res = requests.get('https://tonari-it.com')
-print(res.text)
+res = requests.get('https://tonari-it.com')#Responseオブジェクト取得
+# print(res.text) #text属性の表示
+with open('./html/tonari-it.html','w') as file: # with構文を使いhtmlファイルを書き込むために開く
+        file.write(res.text) # text属性を書き込む
+
+
