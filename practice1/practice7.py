954c7e86821fbdfa7ead6119f1e1e54a92a52748
switch <shg18.sk1.hr10@gmail.com>
2018-08-16 22:36:52 +0900

    記事タイトルとリンクURLを出力するスクリプトを追加

---
+++ practice1/practice7.py
@@ -0,0 +1,11 @@
+"""
+記事タイトルとリンクURLを出力するスクリプト
+
+"""
+import requests, bs4
+res = requests.get('https://tonari-it.com')
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text, "html.parser")
+elems = soup.select('#list h2 a')
+for elem in elems:
+    print('{} ({})'.format(elem.getText(), elem.get('href')))
